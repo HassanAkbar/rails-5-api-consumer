6caedae1d62aebebfc050e3100acc913912ba63f
Hassan <hassanakbar2@gmail.com>
2019-01-26 16:24:13 +0500

    caching added

--- rails-awesome-api/app/controllers/contents_controller.rb
+++ rails-awesome-api/app/controllers/contents_controller.rb
@@ -1,6 +1,8 @@
 class ContentsController < ApplicationController
   def index
-    @contents = Content.published
+    @contents = Rails.cache.fetch("published_contents", expires_in: 1.minute) do  
+      Content.published
+    end
     respond_to do |format|
       format.json { render json: @contents, each_serializer: ContentSerializer, status: 200 }
     end
