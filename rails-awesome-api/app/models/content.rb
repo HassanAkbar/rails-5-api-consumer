4f2dda6b37acef4bd5fdd6066ef23c29bf40350e
Hassan <hassanakbar2@gmail.com>
2019-01-26 17:03:44 +0500

    remove a comment

--- rails-awesome-api/app/models/content.rb
+++ rails-awesome-api/app/models/content.rb
@@ -1,5 +1,4 @@
 class Content < ApplicationRecord
   scope :published, -> { where(status: :published) }
   scope :draft, -> { where(status: :draft) }
-  #validates_presence_of :title, :created_by
 end
